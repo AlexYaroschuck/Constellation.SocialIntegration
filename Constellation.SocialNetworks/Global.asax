81a385158068419378fbaa1d407b51818c3f5612
holla-dolla <Alexyaroschuck@yandex.ua>
2016-04-18 20:57:04 +0300

    first commit

---
+++ Constellation.SocialNetworks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Constellation.SocialNetworks.MvcApplication" Language="C#" %>
---
+++ Constellation.SocialNetworks/Scripts/SocialIntegration/VkIntegration.js
@@ -0,0 +1,23 @@
+﻿VK.init({
+    apiId: 5352309
+});
+function authInfo(response) {
+    if (response.session) {
+        for (var el in response.session)
+            console.log(el["user.id"]);
+    } else {
+        alert('not auth');
+    }
+}
+// VK.Auth.getLoginStatus(authInfo);
+
+$("#VkGetNews").click(function () {
+    VK.Api.call('newsfeed.get', { count: 100 }, function (r) {
+        if (r.response) {
+            for (var el in r.response) {
+                console.log(el);
+            }
+        }
+    });
+});
+VK.UI.button('login_button');
